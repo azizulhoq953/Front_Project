ed13a78571762c22546c36a34351fbe4af331dab
azizulhoq953 <azizulhoq2041@gmail.com>
2024-04-14 12:28:35 +0600

    footer done

---
+++ lxroot/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            width: {
+                'custom-40': '90%', // Custom width of 40%
+                'custom-200px': '200px', // Custom width of 200 pixels
+            },
+        },
+    },
+    plugins: [],
+};
